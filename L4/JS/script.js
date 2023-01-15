6e9ba6923f0f8066002e53adc40045584f431114
Arsen Basarab <arsenbsrb@gmail.com>
2023-01-15 23:15:59 +0200

    u

--- L4/JS/script.js
+++ L4/JS/script.js
@@ -2,42 +2,46 @@ $(loadCategories());
 
 // rand Category
 // use func loadCatalogs
-function randCatalog() {
+function randCatalog(categoryShortName, link) {
   //hide first div or remove after append using `$(".card:first").remove()`
-  $(".category").hide()//With no parameters, the .hide() method is the simplest way to hide an element
+  $(".category").hide()
   $(".catalog").hide()
   $.ajax({
-    url: "data/categories.json",
+    url: link,
     success: function(result) {
-      $.each(result, function(index, item) {
-
-        //var categories = $(".category:first").clone() //clone first divs
+      $.each(result.catalog_items, function(index, item) {
+        
+        var catalogs = $(".catalog:first").clone() //clone first divs
         //var id = item.id;
         //var shortName = item.short_name;
         //var name = item.name;
-        //var instruction = item.special_instruction;
-        //var url = item.url;
+        //var description = item.description;
+        //var price_retail = item.price_retail;
+        //var price_wholesale = item.price_wholesale;
+        //var amount_retail = item.amount_retail;
+        //var amount_wholesale = item.amount_wholesale;
+
         const keys = Object.keys(item);
         const randIndex = Math.floor(Math.random() * keys.length);
         const randKey = keys[randIndex];
         const datas = item[randKey];
+
+        //add values inside divs
+        $(catalogs).find(".catalog-short_name").html(datas[i].short_name);
+        $(catalogs).find(".catalog-name").html(datas[i].name);
+        $(catalogs).find(".catalog-description").html(datas[i].description);
+        $(catalogs).find(".catalog-price_retail").html(datas[i].price_retail);
+        $(catalogs).find(".catalog-price_wholesale").html(datas[i].price_wholesale);
+        $(catalogs).find(".catalog-amount_retail").html(datas[i].amount_retail);
+        $(catalogs).find(".catalog-amount_wholesale").html(datas[i].amount_wholesale);
         
-        for (let i = 0; i < datas.length; i++) {
-          //add values inside divs
-          //$(categories).find(".category-short_name").html(datas[i].short_name);
-          //$(categories).find(".category-name").html(datas[i].name);
-          //$(categories).find(".category-instruction").html(datas[i].special_instruction);
-          //$(categories).find(".category-img").on("click", function() {
-            loadCatalogs(datas[i].short_name, datas[i].url);
-          //});
-          //$(categories).find(".category-img").attr("src", "images/categories/" + datas[i].short_name + ".jpg");
-          $(categories).show() //show cards
-          $(categories).appendTo($(".container")) //append to container
-        }
-        
+        //$(catalogs).find(".catalog-img").attr("src", "images/catalog/" + categoryShortName + "/" + datas[i].short_name + ".jpg");
+        $(catalogs).show() //show cards
+        $(catalogs).appendTo($(".container")) //append to container
       });
     }
-  });
+  })
+
 }
 
 function loadCategories() {
